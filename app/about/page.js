ee99ba5de248f949ffdb2c27251cb8feddebe294
vpsingh16 <144575378+vpsingh16@users.noreply.github.com>
2025-02-05 11:41:52 +0530

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 const page = () => {
   return (
     <div>
       This is about section of this page. 
       <div>
-      In this project I am just focussing on backend part will make a full fledge application <b>thanks chat</b> with all pages well defined
+      In this project I am just focussing on backend part will make a full fledge application <b><Link target='_blank'  href="https://thanks-chat.vercel.app/">thanks chat</Link></b> with all pages well defined
       </div>
     </div>
   )

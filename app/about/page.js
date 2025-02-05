028fb50a2d2120017311c05060fa1467c0be45ea
vpsingh16 <144575378+vpsingh16@users.noreply.github.com>
2025-02-05 11:11:59 +0530

    Add files via upload

---
+++ app/about/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      This is about section of this page. 
+      <div>
+      In this project I am just focussing on backend part will make a full fledge application <b>thanks chat</b> with all pages well defined
+      </div>
+    </div>
+  )
+}
+
+export default page
---
+++ app/contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-center py-10'>
+      Hey I am Varun Pratap Singh you can contact me on whatsapp : 9911913991
+    </div>
+  )
+}
+
+export default page

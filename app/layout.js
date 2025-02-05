c68b51de245d89817bb6ca2fb3dfec36d7e6fd53
vpsingh16 <144575378+vpsingh16@users.noreply.github.com>
2025-02-05 12:48:37 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -15,8 +15,8 @@ const geistMono = localFont({
 });
 
 export const metadata = {
-  title: "Bitlinks - Your trusted URL shortener",
-  description: "Bitlinks helps you shorten your Urls easily",
+  title: "Shortify - Your trusted URL shortener",
+  description: "Shortify helps you shorten your Urls easily",
 };
 
 export default function RootLayout({ children }) {
